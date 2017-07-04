070256997841e7d74fe4888b7c21eee1a079b61c
CRYDEsigN <Cryden_da@mail.ru>
2017-07-04 15:51:15 +0300

    set up Gentelella Admin

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -5,17 +5,31 @@
 @endsection
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
 
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+<div class="x_panel">
+    <div class="x_title">
+        <h2>Main</h2>
+            <ul class="nav navbar-right panel_toolbox">
+                <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="#">Settings 1</a></li>
+                        <li><a href="#">Settings 2</a></li>
+                    </ul>
+                </li>
+                <li><a class="close-link"><i class="fa fa-close"></i></a></li>
+            </ul>
+            <div class="clearfix"></div>
     </div>
+    <div class="x_content">
+        <div class="row">
+            You are logged in!
+        </div>
+     </div>
 </div>
+
 @endsection
+
+
--- routes/web.php
+++ routes/web.php
@@ -21,14 +21,16 @@ Route::get('/', function () {
 Auth::routes();
 
 // OAuth Routes
-Route::get('/auth/{provider}', 			'Auth\AuthController@redirectToProvider');
+Route::get('/auth/{provider}', 				'Auth\AuthController@redirectToProvider');
 Route::get('/auth/{provider}/callback', 	'Auth\AuthController@handleProviderCallback');
 
 // User routes
 
 Route::get('/user/profile', 				'User\UserController@userProfile')->name('profile');
-Route::get('/user/{user_id}', 			'User\UserController@userInfo');
+Route::get('/user/{user_id}', 				'User\UserController@userInfo');
 
 // Dashboard routes
-Route::get('/admin', 					'Admin\HomeController@index')->name('home');
-Route::get('/admin/users', 					'User\UserController@index');
+Route::group(['middleware' => 'auth', 'prefix' => 'admin'], function () {
+	Route::get('/', 'Admin\HomeController@index')->name('home');
+	Route::get('/users', 'User\UserController@index');
+});
